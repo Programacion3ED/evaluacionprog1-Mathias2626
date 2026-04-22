c97e467ee956061dfbc429aabb6972793338d052
Laboratorio <laboratorio@udla-ec.int>
2026-04-22 11:17:35 -0500

    Mathias Zea v3

---
+++ src/main/java/UsuarioSeguroAvanzado.java
@@ -0,0 +1,78 @@
+public class UsuarioSeguroAvanzado {
+    private String username;
+    private String password;
+    private int intentosFallidos;
+    private boolean bloqueado;
+    private int maxIntentos;
+    private boolean accesoExitoso;
+    public UsuarioSeguroAvanzado(String username, String password, int maxIntentos) {
+        this.username = username;
+        this.password = password;
+        this.intentosFallidos = 0;
+        this.bloqueado = false;
+        this.accesoExitoso = false;
+        this.maxIntentos = (maxIntentos <= 0) ? 3 : maxIntentos;
+    }
+    // Métodos accesorios
+    public String getUsername() {
+        return username;
+    }
+    public int getIntentosFallidos() {
+        return intentosFallidos;
+    }
+    public boolean isBloqueado() {
+        return bloqueado;
+    }
+    public int getMaxIntentos() {
+        return maxIntentos;
+    }
+    public boolean isAccesoExitoso() {
+        return accesoExitoso;
+    }
+    // Métodos de negocio
+    public boolean autenticar(String passwordIngresada) {
+        if (bloqueado) {
+            return false;
+        }
+        if (password.equals(passwordIngresada)) {
+            intentosFallidos = 0;
+            accesoExitoso = true;
+            return true;
+        } else {
+            intentosFallidos++;
+            if (intentosFallidos >= maxIntentos) {
+                bloqueado = true;
+            }
+            return false;
+        }
+    }
+    public void reiniciarAcceso() {
+        intentosFallidos = 0;
+        bloqueado = false;
+    }
+    public boolean cambiarPassword(String actual, String nueva) {
+        if (bloqueado) {
+            return false;
+        }
+        if (!password.equals(actual)) {
+            return false;
+        }
+        if (!validarPasswordSegura(nueva)) {
+            return false;
+        }
+        password = nueva;
+        return true;
+    }
+    public boolean validarPasswordSegura(String nueva) {
+        if (nueva.length() < 8) {
+            return false;
+        }
+        boolean tieneMayuscula = false;
+        boolean tieneNumero = false;
+        for (char c : nueva.toCharArray()) {
+            if (Character.isUpperCase(c)) tieneMayuscula = true;
+            if (Character.isDigit(c)) tieneNumero = true;
+        }
+        return tieneMayuscula && tieneNumero;
+    }
+}
